90fe2e9f2176cc30df0e2965cc92ed5f007b1230
pwm001 <philip.michael@scekc.com>
2016-12-27 16:19:49 -0600

    Back to zero

--- Module1.vb
+++ Module1.vb
@@ -2,19 +2,6 @@
 
     Sub Main()
         Console.WriteLine("This is a test")
-        Console.WriteLine("This is a test 2")
-        Console.WriteLine("This is a test 3")
-        Console.WriteLine("This is a test 4")
-        Console.WriteLine("This is a test 5")
-        Console.WriteLine("This is a test 6")
-        Console.WriteLine("This is a test 7")
-        Console.WriteLine("This is a test 8")
-        Console.WriteLine("This is a test 9")
-        Console.WriteLine("This is a test 10")
-        Console.WriteLine("This is a test 11")
-        Console.WriteLine("This is a test 12")
-        Console.WriteLine("This is a test 13")
-        Console.WriteLine("This is a test 14")
 
     End Sub
 
