bb44c8a32f87c917e9302a696b7d12892d341802
pwm001 <philip.michael@scekc.com>
2016-12-27 13:12:55 -0600

    Added 4th line

--- Module1.vb
+++ Module1.vb
@@ -4,6 +4,7 @@
         Console.WriteLine("This is a test")
         Console.WriteLine("This is a test 2")
         Console.WriteLine("This is a test 3")
+        Console.WriteLine("This is a test 4")
     End Sub
 
 End Module
