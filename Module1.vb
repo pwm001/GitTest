f78639da59dccf02d84215253662e6ef37bb5335
pwm001 <philip.michael@scekc.com>
2016-12-27 11:58:15 -0600

    Initial
    This is a test

---
+++ Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("This is a test")
+        Console.WriteLine("This is a test 2")
+        Console.WriteLine("This is a test 3")
+    End Sub
+
+End Module
